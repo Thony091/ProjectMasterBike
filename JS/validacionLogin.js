34142a8ec61f0c6ed68c0f74282b6409103acb9d
ThonyFlores <anthony.g91@icloud.com>
2023-05-29 08:15:27 -0400

    avance validaciones

--- JS/validacionLogin.js
+++ JS/validacionLogin.js
@@ -2,37 +2,42 @@ document.addEventListener("DOMContentLoaded", function () {
     let form = document.querySelector("#form-login");
     let user = document.querySelector("#user");
     let password = document.querySelector("#password"); 
-
+    
 
     function validateLogin() {
         let userValue = user.value;
         let passwordValue = password.value;
 
-        // if (!userValue.isEqual("admin") ) {
-           
-        //     document.querySelector("#finalHelp").innerHTML = "Usuario o contraseña incorrecto.";
-        // } else if (userValue.length == 0){
-
-        // }
         
         if(userValue.length == 0 || passwordValue.length == 0){
             
             document.querySelector("#finalHelp").innerHTML = "Debe Ingresar una Cuenta Valida.";
+        }else{
+            document.querySelector("#finalHelp").innerHTML = " ";
+            
+        }
+        
+        if (userValue != "admin" || passwordValue != "admin" ) {
+            
+            document.querySelector("#finalHelp").innerHTML = "Usuario o contraseña incorrecto.";
             return false;
+        } else{
+            document.querySelector("#finalHelp").innerHTML = " ";
         }
 
         return true;
     }
+
+    // document.querySelector("#boton").onclick = validateLogin;
+    let validatebuttom = document.querySelector("#boton").onclick = validateLogin;
+    
     form.addEventListener("submit", function (event) {
-        if (!validateLogin) {
+        if (!validatebuttom) {
             event.preventDefault();
+        }else{
+            alert("Usuario existente.")
         }
     })
 
 
 })
-
-function isEqual(str1, str2)
-{
-  return str1.toUpperCase() === str2.toUpperCase()
-}
